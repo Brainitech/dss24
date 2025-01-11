e4db90fd50f677825ce68094a65af3b9dd4707c5
Brainitech <gaming.brainiac2020@gmail.com>
2025-01-11 14:01:05 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
-import express from 'express';
-import dotenv from "dotenv";
-import cors from "cors";
-import { connectDB } from './config/mongodb.config.js';
-import userRouter from './routes/user.routes.js';
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import { connectDB } from "./config/mongodb.config.js"
+import userRouter from "./routes/user.routes.js"
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+const app = express()
+const PORT = process.env.PORT || 8000
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json()); //The json response from FE to BE is parsed using this
-app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded form data
+app.use(express.json()) //The json response from FE to BE is parsed using this
+app.use(express.urlencoded({ extended: true })) // Middleware to parse URL-encoded form data
 
-connectDB();
+connectDB()
 
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1/user", userRouter)
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+  res.send(" World!")
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`)
 })
